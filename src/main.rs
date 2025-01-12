f88dd52683253dc3c0c36e37052e50d73b4adf8a
Vasyl Kuzmyk <vasyl.kuzmyk@protonmail.com>
2025-01-12 21:37:16 +0000

    Move displaying to the main module (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 mod feed_parser;
 
 fn main() {
-    feed_parser::main::main();
+    match feed_parser::main::main() {
+        Ok(entries) => {
+            for entry in entries {
+                println!("Title: {}", entry.title);
+                println!("Link: {}", entry.link);
+                println!("Updated: {}", entry.updated);
+                println!("Author: {}", entry.author.name);
+                println!();
+            }
+        }
+        Err(error) => {
+            eprintln!("Error: {}", error);
+        }
+    }
 }
