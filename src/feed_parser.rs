b343c24b105e8a031b052663aeb54b0b1cd2ffc2
TheBlackArroVV <vasyl.kuzmyk@protonmail.com>
2025-01-21 00:11:04 +0000

    Add tests

--- src/feed_parser.rs
+++ src/feed_parser.rs
@@ -1,23 +1,22 @@
-use quick_xml::Reader;
 use quick_xml::events::Event;
+use quick_xml::Reader;
 use serde::Deserialize;
 use std::error::Error;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct Entry {
     pub title: String,
     pub link: String,
     pub updated: String,
-    pub author: Author
+    pub author: Author,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct Author {
-    pub name: String
+    pub name: String,
 }
 
 pub type ParserError = Box<dyn Error>;
-
 #[tokio::main]
 pub async fn feed_parser(url: String) -> Result<Vec<Entry>, ParserError> {
     match read_feed(url).await {
@@ -32,7 +31,7 @@ fn empty_entry() -> Entry {
         link: String::new(),
         updated: String::new(),
         author: Author {
-            name: String::new()
+            name: String::new(),
         },
     }
 }
@@ -51,45 +50,40 @@ async fn read_feed(url: String) -> Result<Vec<Entry>, ParserError> {
     }
 }
 
-fn parse_entries(reader: &mut Reader<&[u8]>, entries: &mut Vec<Entry>) -> Option<Result<Vec<Entry>, ParserError>> {
+fn parse_entries(
+    reader: &mut Reader<&[u8]>,
+    entries: &mut Vec<Entry>,
+) -> Option<Result<Vec<Entry>, ParserError>> {
     let mut current_entry = empty_entry();
     let mut buf = Vec::new();
 
     loop {
         match reader.read_event(&mut buf) {
             Ok(Event::Start(ref e)) => match e.name() {
-                b"title" => {
-                    match reader.read_event(&mut buf) {
-                        Ok(Event::Text(e)) => {
-                            current_entry.title = e.unescape_and_decode(&reader).unwrap();
-                        }
-                        _ => (),
+                b"title" => match reader.read_event(&mut buf) {
+                    Ok(Event::Text(e)) => {
+                        current_entry.title = e.unescape_and_decode(&reader).unwrap();
                     }
-                }
-                b"updated" => {
-                    match reader.read_event(&mut buf) {
-                        Ok(Event::Text(e)) => {
-                            current_entry.updated = e.unescape_and_decode(&reader).unwrap();
-                        },
-                        _ => (),
+                    _ => (),
+                },
+                b"updated" => match reader.read_event(&mut buf) {
+                    Ok(Event::Text(e)) => {
+                        current_entry.updated = e.unescape_and_decode(&reader).unwrap();
                     }
-                }
-                b"author" => {
-                    match reader.read_event(&mut buf) {
-                        Ok(Event::Start(ref e)) => match e.name() {
-                            b"name" => {
-                                match reader.read_event(&mut buf) {
-                                    Ok(Event::Text(e)) => {
-                                        current_entry.author.name = e.unescape_and_decode(&reader).unwrap();
-                                    },
-                                    _ => (),
-                                }
+                    _ => (),
+                },
+                b"author" => match reader.read_event(&mut buf) {
+                    Ok(Event::Start(ref e)) => match e.name() {
+                        b"name" => match reader.read_event(&mut buf) {
+                            Ok(Event::Text(e)) => {
+                                current_entry.author.name = e.unescape_and_decode(&reader).unwrap();
                             }
                             _ => (),
                         },
                         _ => (),
-                    }
-                }
+                    },
+                    _ => (),
+                },
                 _ => (),
             },
             Ok(Event::End(ref e)) => {
@@ -101,14 +95,19 @@ fn parse_entries(reader: &mut Reader<&[u8]>, entries: &mut Vec<Entry>) -> Option
             Ok(Event::Eof) => break,
             Ok(Event::Empty(ref e)) => {
                 if e.name() == b"link" {
-                    match e.attributes().with_checks(false).find(|a| a.as_ref().unwrap().key == b"href") {
+                    match e
+                        .attributes()
+                        .with_checks(false)
+                        .find(|a| a.as_ref().unwrap().key == b"href")
+                    {
                         Some(attr) => {
-                            current_entry.link = attr.unwrap().unescape_and_decode_value(&reader).unwrap();
-                        },
+                            current_entry.link =
+                                attr.unwrap().unescape_and_decode_value(&reader).unwrap();
+                        }
                         _ => (),
                     }
                 }
-            },
+            }
             Err(e) => return Some(Err(Box::new(e))),
             _ => (),
         }
--- src/file_utils.rs
+++ src/file_utils.rs
@@ -1,8 +1,75 @@
+use std::{
+    error::Error,
+    fs::{self, File},
+    path::Path,
+};
+
+use crate::feed_parser::ParserError;
+
 pub fn config_file_location() -> String {
     format!("{}/rss_links.yml", config_directory_location())
 }
 
-pub fn config_directory_location() -> String {
+pub fn config_file() -> String {
+    let file = config_file_location();
+
+    if Path::new(&file).exists() {
+        return file;
+    }
+
+    fs::create_dir_all(config_directory_location()).unwrap();
+
+    File::create(&file).expect("unable to create file");
+
+    file
+}
+
+pub fn read_file(file_location: String) -> Result<Vec<String>, Box<dyn Error>> {
+    let data: Vec<u8> = fs::read(file_location).unwrap();
+
+    match String::from_utf8(data) {
+        Ok(data) => {
+            let mut links: Vec<String> = Vec::new();
+            for line in data.lines() {
+                links.push(line.to_string());
+            }
+            Ok(links)
+        }
+        Err(error) => Err(ParserError::from(error)),
+    }
+}
+
+fn config_directory_location() -> String {
     let home_directory_path = std::env::var("HOME").expect("no home directory");
     format!("{}/.config/yarr", home_directory_path)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::file_utils::{config_file_location, read_file};
+
+    #[test]
+    fn config_file_location_is_correct() {
+        let home_directory_path = std::env::var("HOME").expect("no home directory");
+        assert_eq!(
+            config_file_location(),
+            format!("{}/.config/yarr/rss_links.yml", home_directory_path)
+        )
+    }
+
+    #[test]
+    fn links_are_properly_parsed() {
+        assert_eq!(
+            read_file(format!(
+                "{}/src/test_links.yml",
+                std::env::current_dir().unwrap().display()
+            ))
+            .unwrap(),
+            vec![
+                "http://first_link.com/".to_string(),
+                "http://second_link.com/".to_string(),
+                "http://third_link.com/".to_string(),
+            ]
+        );
+    }
 }
